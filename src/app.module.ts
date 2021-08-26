2f3ebdc73720fa5666bb771f4c0240edbce1710c
Mincho Milev <mincho.milev@gmail.com>
2021-08-26 16:17:11 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://minchomilev:Samoenter123@cluster0.pfnuj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
