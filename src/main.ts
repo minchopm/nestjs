120af1b743c10e7823920b029898cbf924b9db42
Mincho Milev <mincho.milev@gmail.com>
2021-08-26 23:51:47 +0300

    try cors

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { urlencoded, json } from 'express';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {json, urlencoded} from 'express';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  const corsOptions = {
-      origin: '*',
-      optionsSuccessStatus: 200,
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  };
-  app.register(require('fastify-cors'), corsOptions);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.use(json({ limit: '500mb' }));
   app.use(urlencoded({ extended: true, limit: '500mb' }));
   await app.listen(8000);
